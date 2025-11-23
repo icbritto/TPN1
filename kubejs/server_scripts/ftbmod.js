e71b214c550e44db10203b99921c52356cfd7f22
icbritto <icbritto.pers@gmail.com>
2025-11-23 17:11:43 -0300

    Updated gitignore

--- kubejs/server_scripts/ftbmod.js
+++
@@ -1,5 +0,0 @@
-ServerEvents.tags('item', event => {
-  // Get the #ftbultimine:tools tag collection and add Atomic Disassembler to it
-  event.add('#ftbultimine:tools', 'mekanism:atomic_disassembler')
-    }
-)
